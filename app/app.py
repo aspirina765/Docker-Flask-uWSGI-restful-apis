61cb46b8548325e0fccda94a8cacdce490e0d0e9
aspirina765 <molinalucas765@gmail.com>
2022-08-04 16:41:16 -0300

    refact

--- app/app.py
+++ app/app.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-incomes_r = [{'description': 'rating', 'amount': 'AA'}]
+incomes_r = [{'description': 'r', 'amount': 'AA'}]
 incomes_dly = [{'description': 'dly', 'amount': '0.030'}]
-incomes_dft = [{'description': 'dft', 'amount': '0.080'}]
+incomes_d = [{'description': 'd', 'amount': '0.080'}]
 
-@app.route('/rating',methods=["POST", "GET"])
-def get_rating():
+@app.route('/r',methods=["POST", "GET"])
+def get_r():
   content = []
   if request.method == "GET":
     content = incomes_r
@@ -16,16 +16,16 @@ def get_rating():
   return jsonify(content)
 
 
-@app.route('/default',methods=["POST", "GET"])
-def get_default():
+@app.route('/d',methods=["POST", "GET"])
+def get_d():
     content = []
     if request.method == "GET":
-      content = incomes_dft
+      content = incomes_d
     return jsonify(content)
 
 
-@app.route('/delay',methods=["POST", "GET"])
-def get_delay():
+@app.route('/dly',methods=["POST", "GET"])
+def get_dly():
     content = []
     if request.method == "GET":
       content = incomes_dly
